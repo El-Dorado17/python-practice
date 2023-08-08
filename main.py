e64ce0f77b60f458833812afdcbe462190abb5fe
Ely Dorado <2018dorado@gmail.com>
2023-08-08 17:33:41 -0500

    data types

--- main.py
+++ main.py
@@ -1,10 +1,61 @@
 
+#? Data Types:
+#Numbers
+integer = 20
 
-name = "Ely"
+float = 3.14
 
-age = 23
+complex = 1+3j  #j used for imaginary numbers
 
-print(name)
+#Boolean
+i_love_to_code = True
 
-for counter in range (1, 10):
-    print("test")
+#set
+set = {2, 4, 6}
+
+#Dictionary
+dictionary = {
+    1: 'One',
+    2: 'Two',
+    3: 'Three'
+}
+
+#Sequences
+list = ['Ely', 1, 2, 23, 74.6]
+string = "Hello, World!"
+
+
+tuple = (3, 3.45,'m')
+
+
+
+
+
+#? Conditionals:
+
+day = 'Wednesday'
+
+if day == 'Wednesday':
+    print ("It's Wednesday, my dudes")
+elif day == 'Friday':
+    print ("T.G.I.F.")
+else:
+    print("it's just another day")
+
+
+
+
+    months = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December'
+}
